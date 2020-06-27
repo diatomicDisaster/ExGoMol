01f4e55b4d729592e8fb166a212b49593f2b39c0
diatomicDisaster <wilfrid.somogyi.15@ucl.ac.uk>
2020-06-27 13:19:56 +0100

    Tidying up

--- linelist.py
+++ linelist.py
@@ -4,21 +4,20 @@ def _read_space_delimited(filename, header_dict):
     """Read a space delimited data file with the first row as headers.
         filename : str
             Name of file to read.
+        header_dict : dict
+            Dictionary of file headers and data types.
     returns
         df : pandas.DataFrame
             Dataframe of the file's contents
     """
-    headers = [_ for _ in header_dict]
-    use_columns, _ = _detect_file_headers(filename, headers)
-    column_inds    = [column[1] for column in use_columns]
-    column_names   = [column[0] for column in use_columns]
-    column_types   = {header : header_dict[header] for header in column_names}
+    headers = [_ for _ in header_dict] #list of header strings
+    use_columns, _ = _detect_file_headers(filename, headers) #list of known headers
     df = pd.read_csv(filename,
         delim_whitespace=True,
-        header=0,
+        header=0, #0-th row as headers
         skip_blank_lines=True,
-        usecols=column_inds,
-        dtype=column_types)
+        usecols=[column[1] for column in use_columns],
+        dtype={column[0] : header_dict[column[0]] for column in use_columns})
     return df
 
 def _detect_file_headers(filename, headers_to_detect):
@@ -28,44 +27,46 @@ def _detect_file_headers(filename, headers_to_detect):
         heads_to_detect : list-like of strings 
             List of header strings to detect.
     returns
-        use_these_columns : list of ints or None
-            List of recognised column heaers and their indices.
+        use_these_columns : list of lists
+            List of recognised column headers (str) and their indices (int).
         garbage : list of list
-            List of unrecognised column headers and their indices.
+            List of unrecognised column headers (str) and their indices (int).
     """
-    use_these_columns = []
+    use_these_columns = [] #collect recognised columns
     garbage = []
     with open(filename, 'r') as f:
         line = f.readline().rstrip('\n')
         for w, word in enumerate(line.split()):
-            if word in headers_to_detect:
-                use_these_columns.append([word, w])
+            if word in headers_to_detect: #if recognised word
+                use_these_columns.append([word, w]) #keep word and column index
             else:
                 garbage.append([word, w])
-        if len(use_these_columns) == 0:
+        if len(use_these_columns) == 0: #if none known
             return None, garbage
         else:
             return use_these_columns, garbage
     
 def exomol_to_linelist(states_file=None, trans_file=None):
     """Convert ExoMol states and trans file to Linelist."""
-
     exomol_states_types = {
-        "stateID": int, 
+        "stateID": int, #exomol states files have additional 'stateID' column
         **Linelist.data_types
     }
     exomol_trans_types  = {
-        "stateID_final": int, 
-        'stateID_initial': int, 
+        "stateID_final": int,   #exomol trans files have two 'stateID' columns
+        'stateID_initial': int,
         **Linelist.data_types
     }
+    # Read '.states' and '.trans' files as space delimited
     states_df = _read_space_delimited(states_file, exomol_states_types)
     trans_df  = _read_space_delimited(trans_file, exomol_trans_types)
+    # Match final state in trans file to stateID in states file
     linelist_df_ = trans_df.merge(states_df, 
         left_on="stateID_final",
         right_on="stateID",
         how="inner"
     )
+    # Match initial state in trans file to stateID in state file
     linelist_df = linelist_df_.merge(states_df,
         left_on="stateID_initial",
         right_on="stateID",
@@ -75,6 +76,8 @@ def exomol_to_linelist(states_file=None, trans_file=None):
     return linelist_df
 
 class Linelist:
+    # Linelist dataframe has two of each of these columns: one for the final
+    # state (suffix "_f") and another for the initial state (suffix "_i")
     state_data_types = {
         "energy": float,
         "lifetime": float,
@@ -92,23 +95,26 @@ class Linelist:
         "parity_rotationless": str,
         "electronic_state_no": str
     }
+    # Linelist data frame has one of each of these columns, with data specific
+    # to transitions between the two states
     transition_data_types = {
         "transition_wavenumber": float,
         "einstein_coefficient": float,
         "transition_linestrength": float,
         "transition_intensity": float
     }
-    data_types = {**state_data_types, **transition_data_types}
+    data_types = {**state_data_types, **transition_data_types} #union of dicts
 
     def __init__(self):
         self.dataframe = None
 
-
+# Create Linelist object and read dataframe from Exomol format
 testLinelist = Linelist()
 testLinelist.dataframe = exomol_to_linelist(
     states_file = "testfiles/test.states",
     trans_file="testfiles/test.trans")
 
+# Just for testing
 with open("blah.txt", 'w') as f:
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
